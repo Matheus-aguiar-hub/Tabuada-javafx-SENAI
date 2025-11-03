08ae78016a4c8ad2176a9a2106ebbe81b27f170e
MATHEUS PEREIRA DE AGUIAR <25204557@cfp127aluno.local>
2025-11-03 11:28:26 -0300

    Começo do projeto

---
+++ src/main/java/br/senai/sp/jandira/tabuadads1m/Launcher.java
@@ -0,0 +1,9 @@
+package br.senai.sp.jandira.tabuadads1m;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module br.senai.sp.jandira.tabuadads1m {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens br.senai.sp.jandira.tabuadads1m to javafx.fxml;
+    exports br.senai.sp.jandira.tabuadads1m;
+}
